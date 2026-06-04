e7fecefdf0ebd133edcf2dc5bd26e10029e298e6
pooja152007 <poja.15.2007@gmail.com>
2026-06-04 16:43:03 +0530

    ver2

--- calc.py
+++ calc.py
@@ -1,4 +1,29 @@
-print("add",2+2);
-print("subtract",2-2);
-print("multiply",2*2);
-print("divide",2/2); 
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def calculator():
+    result = ""
+
+    if request.method == "POST":
+        num1 = float(request.form["num1"])
+        num2 = float(request.form["num2"])
+        operation = request.form["operation"]
+
+        if operation == "+":
+            result = num1 + num2
+        elif operation == "-":
+            result = num1 - num2
+        elif operation == "*":
+            result = num1 * num2
+        elif operation == "/":
+            if num2 != 0:
+                result = num1 / num2
+            else:
+                result = "Cannot divide by zero"
+
+    return render_template("index.html", result=result)
+
+if __name__ == "__main__":
+    app.run(debug=True) 
