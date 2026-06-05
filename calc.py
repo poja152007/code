06abc6f5765c96b2520c927c24c4c2a4f0332774
pooja152007 <poja.15.2007@gmail.com>
2026-06-05 17:03:51 +0530

    ver5

--- calc.py
+++ calc.py
@@ -26,5 +26,5 @@ def calculator():
     return render_template("index.html", result=result)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=6000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
      
